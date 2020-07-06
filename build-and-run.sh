fe354bcad8ce59f85700c2135e611583dad86003
JP Silva <jplfdsilva@gmail.com>
2020-07-06 12:18:43 -0400

    accepts number of products as a parameter

---
+++ build-and-run.sh
@@ -0,0 +1,1 @@
+lein uberjar && java -jar target/uberjar/clojure-async-sandbox-0.1.0-SNAPSHOT-standalone.jar
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar target/uberjar/clojure-async-sandbox-0.1.0-SNAPSHOT-standalone.jar $1
