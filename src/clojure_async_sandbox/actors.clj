89d9caee21a3c52b32156d61278a10b2899d3e68
JP Silva <jplfdsilva@gmail.com>
2020-11-23 06:26:07 -0500

    Extracts actor core behavior into its own namespace

---
+++ src/clojure_async_sandbox/actors.clj
@@ -0,0 +1,13 @@
+(ns clojure-async-sandbox.actors
+  (:require [clojure.core.async :as async])
+  (:require [while-let.core :refer :all]))
+
+(defn actor [behavior]
+  (let [input (atom (async/chan))]
+    (async/go
+      (let [current-input @input
+            message (async/<! @input)]
+        (swap! input (fn update-input [_] (behavior message)))
+        (while-let [m (async/<! current-input)]
+                   (async/>! @input m))))
+    @input))
--- src/clojure_async_sandbox/bank_account.clj
+++ src/clojure_async_sandbox/bank_account.clj
@@ -1,31 +1,25 @@
 (ns clojure-async-sandbox.bank-account
   (:require [clojure.core.async :as async])
   (:require [while-let.core :refer :all])
+  (:require [clojure-async-sandbox.actors :refer :all])
   (:gen-class))
 
 (defn bank-account
   ([] (bank-account 0))
   ([current-balance] (bank-account current-balance []))
   ([current-balance transaction-history]
-   (let [input (atom (async/chan))]
-     (defmulti bank-account-behavior (fn [message] (:type message)))
-     (defmethod bank-account-behavior :credit [message]
-       (bank-account (+ current-balance (:amount message)) (conj transaction-history message)))
-     (defmethod bank-account-behavior :debit [message]
-       (bank-account (- current-balance (:amount message)) (conj transaction-history message)))
-     (defmethod bank-account-behavior :current-balance [message]
-       (println "Current balance is:" current-balance)
-       (bank-account current-balance transaction-history))
-     (defmethod bank-account-behavior :list-transactions [message]
-       (doseq [transaction transaction-history] (println "Transaction:" (:type transaction) (:amount transaction)))
-       (bank-account current-balance transaction-history))
-     (async/go
-       (let [current-input @input
-             message (async/<! @input)]
-                  (swap! input (fn update-input [_] (bank-account-behavior message)))
-                  (while-let [m (async/<! current-input)]
-                             (async/>! @input m))))
-     @input)))
+   (defmulti bank-account-behavior (fn [message] (:type message)))
+   (defmethod bank-account-behavior :credit [message]
+     (bank-account (+ current-balance (:amount message)) (conj transaction-history message)))
+   (defmethod bank-account-behavior :debit [message]
+     (bank-account (- current-balance (:amount message)) (conj transaction-history message)))
+   (defmethod bank-account-behavior :current-balance [message]
+     (println "Current balance is:" current-balance)
+     (bank-account current-balance transaction-history))
+   (defmethod bank-account-behavior :list-transactions [message]
+     (doseq [transaction transaction-history] (println "Transaction:" (:type transaction) (:amount transaction)))
+     (bank-account current-balance transaction-history))
+   (actor bank-account-behavior)))
 
 
 (defn -main [& args]
