c5bea606cc9d33688add196ed66905ddfe24b8bf
JP Silva <jplfdsilva@gmail.com>
2020-11-23 06:10:58 -0500

    Removes unnecessary code

--- src/clojure_async_sandbox/actors.clj
+++ src/clojure_async_sandbox/actors.clj
@@ -8,22 +8,21 @@
   ([current-balance] (bank-account current-balance []))
   ([current-balance transaction-history]
    (let [input (atom (async/chan))]
-     (defmulti bank-account-behavior (fn [state message] (:type message)))
-     (defmethod bank-account-behavior :credit [state message]
-       (bank-account (+ (:current-balance state) (:amount message)) (conj (:transaction-history state) message)))
-     (defmethod bank-account-behavior :debit [state message]
-       (bank-account (- (:current-balance state) (:amount message)) (conj (:transaction-history state) message)))
-     (defmethod bank-account-behavior :current-balance [state message]
+     (defmulti bank-account-behavior (fn [message] (:type message)))
+     (defmethod bank-account-behavior :credit [message]
+       (bank-account (+ current-balance (:amount message)) (conj transaction-history message)))
+     (defmethod bank-account-behavior :debit [message]
+       (bank-account (- current-balance (:amount message)) (conj transaction-history message)))
+     (defmethod bank-account-behavior :current-balance [message]
        (println "Current balance is:" current-balance)
-       (bank-account (:current-balance state) (:transaction-history state)))
-     (defmethod bank-account-behavior :list-transactions [state message]
-       (doseq [transaction (:transaction-history state)] (println "Transaction:" (:type transaction) (:amount transaction)))
-       (bank-account (:current-balance state) (:transaction-history state)))
+       (bank-account current-balance transaction-history))
+     (defmethod bank-account-behavior :list-transactions [message]
+       (doseq [transaction transaction-history] (println "Transaction:" (:type transaction) (:amount transaction)))
+       (bank-account current-balance transaction-history))
      (async/go
        (let [current-input @input
              message (async/<! @input)]
-                  (swap! input (fn update-input [_] (bank-account-behavior {:current-balance current-balance
-                                                                            :transaction-history transaction-history} message)))
+                  (swap! input (fn update-input [_] (bank-account-behavior message)))
                   (while-let [m (async/<! current-input)]
                              (async/>! @input m))))
      @input)))
@@ -36,4 +35,4 @@
       (async/>! bank-account {:type :debit :amount 300})
       (async/>! bank-account {:type :current-balance})
       (async/>! bank-account {:type :list-transactions})))
-  (while true))
+  (read-line))
